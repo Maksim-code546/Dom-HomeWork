c595ce8b65dc4f59436270e9c2c46c12c183a2ee
Maksim <maksimzubov709@gmail.com>
2025-11-10 14:31:48 +0500

    GET

--- script/modules/index.js
+++ script/modules/index.js
@@ -4,6 +4,14 @@ import { formatDate, escapeHtml } from './util.js'
 import { renderComments } from './render.js'
 import { setupEventListeners, addComment, cancelReply } from './events.js'
 
+fetch('https://wedev-api.sky.pro/api/v1/Maksim-Zubov/comments')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+    })
+
 export {
     commentsData,
     formatDate,
